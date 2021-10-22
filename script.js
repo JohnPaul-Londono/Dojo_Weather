8068c810ccff9fbc15575a982e31b6dc73613b51
JohnPaul-Londono <johnplondono@gmail.com>
2021-10-22 12:18:32 -0500

    reworked converter

--- script.js
+++ script.js
@@ -4,54 +4,47 @@ function hidecookies(id) {
     element.remove()
 }
 
-// change temp
 
-
-// function onselect(){
-// 	var index = document.getElementById("tempselect").selectedIndex;
-//     switch(index) {
-//     	case 0:
-//         	document.getElementById("red1").innerHTML = '75°';
-//             break;
-//         case 1:
-//         	document.getElementById("p").innerHTML = 'You Currently Have "GREEN" Selected!';
-//             break;
-//       	case 2:
-//         	document.getElementById("p").innerHTML = 'You Currently Have "BLUE" Selected!';
-//             break;
-        	
-//     }
-// }
-
-function ontemp(element){
-   if(element.options[element.selectedIndex].value =='° F'){
-    document.querySelector("#red1").innerText = "75°";
-    document.querySelector("#blue1").innerText = "65°";
-    document.querySelector("#red2").innerText = "80°";
-    document.querySelector("#blue2").innerText = "66°";
-    document.querySelector("#red3").innerText = "69°";
-    document.querySelector("#blue3").innerText = "61°";
-    document.querySelector("#red4").innerText = "78°";
-    document.querySelector("#blue4").innerText = "70°";
-   }
-   else if(element.options[element.selectedIndex].value =='° C'){
-    document.querySelector("#red1").innerText = "24°";
-    document.querySelector("#blue1").innerText = "18°";
-    document.querySelector("#red2").innerText = "27°";
-    document.querySelector("#blue2").innerText = "19°";
-    document.querySelector("#red3").innerText = "21°";
-    document.querySelector("#blue3").innerText = "16°";
-    document.querySelector("#red4").innerText = "26°";
-    document.querySelector("#blue4").innerText = "21°";
-   }
+// the code I sent you
+function onTemp(element) {
+    console.log(element.value);
+    var tempArray = document.querySelectorAll(".temperature");
+    console.log(tempArray)
+    if (element.options[element.selectedIndex].value == '° F') {
+        for (var i = 0; i < tempArray.length; i++) {
+            tempArray[i].innerText = Math.round(tempArray[i].innerText * 9 / 5 + 32);
+        }
+    }
+    else {
+        for (var i = 0; i < tempArray.length; i++) {
+            tempArray[i].innerText = Math.round((tempArray[i].innerText - 32) * 5 / 9);
+        }
+    }
 }
-// function ontempc(){
-//     document.querySelector("#red1").innerHTML = "24°";
-//     document.querySelector("#blue1").innerHTML = "18°";
-//     document.querySelector("#red2").innerHTML = "27°";
-//     document.querySelector("#blue2").innerHTML = "19°";
-//     document.querySelector("#red3").innerHTML = "21°";
-//     document.querySelector("#blue3").innerHTML = "16°";
-//     document.querySelector("#red4").innerHTML = "26°";
-//     document.querySelector("#blue4").innerHTML = "21°";
+
+
+// the first equation I did, very primative and not practical
+// function onTemp(element){
+//    if(element.options[element.selectedIndex].value =='° F'){
+//     document.querySelector("#red1").innerText = "75°";
+//     document.querySelector("#blue1").innerText = "65°";
+//     document.querySelector("#red2").innerText = "80°";
+//     document.querySelector("#blue2").innerText = "66°";
+//     document.querySelector("#red3").innerText = "69°";
+//     document.querySelector("#blue3").innerText = "61°";
+//     document.querySelector("#red4").innerText = "78°";
+//     document.querySelector("#blue4").innerText = "70°";
+//    }
+//    else if(element.options[element.selectedIndex].value =='° C'){
+//     document.querySelector("#red1").innerText = "24°";
+//     document.querySelector("#blue1").innerText = "18°";
+//     document.querySelector("#red2").innerText = "27°";
+//     document.querySelector("#blue2").innerText = "19°";
+//     document.querySelector("#red3").innerText = "21°";
+//     document.querySelector("#blue3").innerText = "16°";
+//     document.querySelector("#red4").innerText = "26°";
+//     document.querySelector("#blue4").innerText = "21°";
+//    }
 // }
+
+
